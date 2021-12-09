d91f50766a97aa1ef10e0edd3389d7c5d9e551a7
Richard Joyal <richardjoyal777@gmail.com>
2021-12-09 23:56:29 +0530

    Breaking best and worst record

---
+++ hackerrank/breaking-best-and-worst-records.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+        int min = -1,max = -1;
+        int min_c = 0,max_c=0;
+        for(int i : scores)
+        {
+            if(min == -1 && max == -1 )
+            {
+                min = i;
+                max = i;
+                continue;
+            }
+            if(i<min)
+            {
+                min = i;
+                min_c++;
+            }
+            if(i>max)
+            {
+                max = i;
+                max_c++;
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        res.add(max_c);
+        res.add(min_c);
+        return res;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> scores = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.breakingRecords(scores);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
