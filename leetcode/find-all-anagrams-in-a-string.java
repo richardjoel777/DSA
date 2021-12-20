6ccc5bf34869c891cc68c520bf9d92671e3c215b
Richard Joyal <richardjoyal777@gmail.com>
2021-12-20 22:29:33 +0530

    Find all anagrams in a string

---
+++ leetcode/find-all-anagrams-in-a-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        char[] sh = new char[26];
+        char[] ph = new char[26];
+        if(p.length()>s.length())
+            return res;
+        int right=0,left=0;
+        for(int i=0;i<p.length();i++)
+        {
+            sh[s.charAt(right)-'a']++;
+            ph[p.charAt(right)-'a']++;
+            right++;
+        }
+        right--;
+        while(right<s.length())
+        {
+            if(Arrays.equals(sh,ph))
+                res.add(left);
+            right++;
+            if(right!=s.length())
+                sh[s.charAt(right)-'a']++;
+            sh[s.charAt(left)-'a']--;
+            left++;
+        }
+        return res;
+    }
+}
