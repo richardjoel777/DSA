1ad19fd7a94978a3dc0598f0aa4dbba3780e9ac6
Richard Joyal <richardjoyal777@gmail.com>
2022-01-05 14:45:25 +0530

    Sum of Root to leaf binary numbers

--- leetcode/sum-of-root-to-leaf-binary-numbers.java
+++ leetcode/sum-of-root-to-leaf-binary-numbers.java
@@ -13,51 +13,71 @@
  *     }
  * }
  */
-class Solution {
+
+
+// class Solution {
     
-    public int res;
+//     public int res;
     
-    public void func(TreeNode root, List<Integer> prev)
-    {
-        if(root.right == null && root.left == null)
-        {
-            prev.add(root.val);
-            String bin = prev.stream().map(Object::toString)
-                        .collect(Collectors.joining(""));
-            System.out.println(prev);
-            res += Integer.parseInt(bin,2);
-            return;
-        }
-        if(root.right != null && root.left != null)
-        {
-            List<Integer> t1 = new ArrayList<Integer>(prev);
-            t1.add(root.val);
-            List<Integer> t2 = new ArrayList<Integer>(prev);
-            t2.add(root.val);
-            func(root.right, t1);
-            func(root.left,t2);
-            return;
-        }
-        if(root.left == null)
-        {
-            List<Integer> t = new ArrayList<Integer>(prev);
-            t.add(root.val);
-            func(root.right,t);
-            return;
-        }
-        if(root.right == null)
-        {
-            List<Integer> t = new ArrayList<Integer>(prev);
-            t.add(root.val);
-            func(root.left,t);
-            return;
-        }
+//     public void func(TreeNode root, List<Integer> prev)
+//     {
+//         if(root.right == null && root.left == null)
+//         {
+//             prev.add(root.val);
+//             String bin = prev.stream().map(Object::toString)
+//                         .collect(Collectors.joining(""));
+//             System.out.println(prev);
+//             res += Integer.parseInt(bin,2);
+//             return;
+//         }
+//         if(root.right != null && root.left != null)
+//         {
+//             List<Integer> t1 = new ArrayList<Integer>(prev);
+//             t1.add(root.val);
+//             List<Integer> t2 = new ArrayList<Integer>(prev);
+//             t2.add(root.val);
+//             func(root.right, t1);
+//             func(root.left,t2);
+//             return;
+//         }
+//         if(root.left == null)
+//         {
+//             List<Integer> t = new ArrayList<Integer>(prev);
+//             t.add(root.val);
+//             func(root.right,t);
+//             return;
+//         }
+//         if(root.right == null)
+//         {
+//             List<Integer> t = new ArrayList<Integer>(prev);
+//             t.add(root.val);
+//             func(root.left,t);
+//             return;
+//         }
         
+//     }
+    
+//     public int sumRootToLeaf(TreeNode root) {
+//         List<Integer> l = new ArrayList<Integer>();
+//         func(root,l);
+//         return res;
+//     }
+// }
+
+
+class Solution {
+    public int sumRootToLeaf(TreeNode root) {
+        return pathSumRootToLeaf(root, 0);
     }
     
-    public int sumRootToLeaf(TreeNode root) {
-        List<Integer> l = new ArrayList<Integer>();
-        func(root,l);
-        return res;
+    private int pathSumRootToLeaf(TreeNode root, int parentNodeSum){
+        if(root == null) return 0;
+        
+        parentNodeSum = 2 * parentNodeSum + root.val;
+        if(root.left == null && root.right == null){
+            return parentNodeSum;
+        }
+        
+        return pathSumRootToLeaf(root.left, parentNodeSum) + pathSumRootToLeaf(root.right, parentNodeSum);
     }
 }
