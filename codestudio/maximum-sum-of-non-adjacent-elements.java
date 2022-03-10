305fd38dc097bf29c3f3f68826ac98ac7594eab3
Richard Joyal <richardjoyal777@gmail.com>
2022-03-10 22:06:16 +0530

    Minimum sum of non adjacent elements

---
+++ codestudio/maximum-sum-of-non-adjacent-elements.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Solution {
+	public static int maximumNonAdjacentSum(ArrayList<Integer> nums) {
+		int n = nums.size();
+        int prev = nums.get(0);
+        int prev1 = 0;
+        for(int i=1;i<n;i++)
+        {
+            int take = nums.get(i);
+            if(i>1)
+                take += prev1;
+            int notTake = 0 + prev;
+            int curr = Math.max(take,notTake);
+            prev1 = prev;
+            prev = curr;
+        }
+        return prev;
+	}
+}
