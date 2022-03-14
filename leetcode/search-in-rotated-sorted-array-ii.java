c0fd678341a1949c235c548a06708d859ce32084
Richard Joyal <richardjoyal777@gmail.com>
2022-03-14 16:39:08 +0530

    Seaech in rotated sorted array ii

---
+++ leetcode/search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        int l = 0;
+        int h = n - 1;
+        while(l<=h)
+        {
+            int mid = l + (h-l)/2;
+            if(nums[mid] == target)
+                return true;
+            if(nums[mid] == nums[h])
+                h--;
+            else if(nums[mid] < nums[h])
+            {
+                if(target > nums[mid] && target <= nums[h])
+                    l = mid+1;
+                else
+                    h = mid-1;
+            }
+            else{
+                if(target >= nums[l] && target < nums[mid])
+                    h = mid-1;
+                else
+                    l = mid+1;
+            }
+        }
+        return false;
+    }
+}
