df126e1376bf889038bffb8ea8ab93b86ac9908d
Richard Joyal <richardjoyal777@gmail.com>
2022-04-05 16:14:51 +0530

    N-Queens - Hashing Method

--- leetcode/n-queens.java
+++ leetcode/n-queens.java
@@ -63,3 +63,51 @@ class Solution {
         return res;
     }
 }
+
+
+
+// HASHING METHOD
+
+// class Solution {
+    
+//     public void f(int col, List<String> l, List<List<String>> res, int n, boolean[] leftRow, boolean[] lowDig, boolean[] upDig)
+//     {
+//         if(col == n)
+//         {
+//             res.add(new ArrayList<>(l));
+//             return;
+//         }
+        
+//         for(int row=0;row<n;row++)
+//         {
+//             if(!leftRow[row] && !lowDig[row+col] && !upDig[n-1+col-row])
+//             {
+//                 StringBuilder temp = new StringBuilder(l.get(row));
+//                 temp.setCharAt(col, 'Q');
+//                 leftRow[row] = true;
+//                 lowDig[row+col] = true;
+//                 upDig[n-1+col-row] = true;
+//                 l.set(row, temp.toString());
+//                 f(col+1, l, res, n, leftRow, lowDig, upDig);
+//                 temp.setCharAt(col,'.');
+//                 l.set(row, temp.toString());
+//                 leftRow[row] = false;
+//                 lowDig[row+col] = false;
+//                 upDig[n-1+col-row] = false;
+//             }
+//         }
+//     }
+    
+//     public List<List<String>> solveNQueens(int n) {
+//         String s = ".".repeat(n);
+//         List<String> l = new ArrayList<>(n);
+//         boolean[] leftRow = new boolean[n];
+//         boolean[] lowDig = new boolean[2*n-1];
+//         boolean[] upDig = new boolean[2*n-1];
+//         for(int i=0;i<n;i++)
+//             l.add(s);
+//         List<List<String>> res = new ArrayList<>();
+//         f(0, l, res, n, leftRow, lowDig, upDig);
+//         return res;
+//     }
+// }
